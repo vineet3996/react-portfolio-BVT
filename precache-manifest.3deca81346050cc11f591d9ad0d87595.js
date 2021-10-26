b5465c9b8c8160583bb0915a0eb7304185944ee4
vineet <tiwari.vin@yahoo.com>
2021-10-26 12:43:08 +0530

    cleanup changes

---
+++ precache-manifest.3deca81346050cc11f591d9ad0d87595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2fd20bb7009040f9d1be190e3dbc1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9da987c193fee4c668b",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f4d1e9ade494fc6c1797",
+    "url": "./static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "b9da987c193fee4c668b",
+    "url": "./static/js/2.2739fe85.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.2739fe85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d1e9ade494fc6c1797",
+    "url": "./static/js/main.d2026533.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
